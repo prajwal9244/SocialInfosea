2ac3fa06a448552690427420da375a6356aa5d1e
prajwal9244 <62738298+prajwal9244@users.noreply.github.com>
2020-03-27 12:15:53 +0530

    Add files via upload

---
+++ groups/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from . import models
+
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+
+
+admin.site.register(models.Group)
